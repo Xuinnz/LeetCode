17cb33ed0ef9fac63ccbb9f0fd656837cd3f0da9
Xuinnz <redtagura07@gmail.com>
2026-02-12 20:50:27 +0800

    refactored to boyer-moore algo

--- 169-majority-element/main.py
+++ 169-majority-element/main.py
@@ -1,14 +1,15 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = {}
-        for i, n in enumerate(nums):
-            if nums[i] in count:
-                count[n] += 1
-            else: 
-                count[n] = 1
-            if count[n] > len(nums) // 2:
-                return n
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = nums[0]
+        i = 0 
+        count = 0
+        while i < len(nums):
+            if count == 0:
+                candidate = nums[i]
+            if candidate == nums[i]:
+                count += 1
+                i += 1
+            else:
+                count -=1
+                i += 1
+        return candidate
